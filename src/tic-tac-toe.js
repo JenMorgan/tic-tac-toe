09da8f7d6f92bc3798c14c49782588af3d8b855e
Julia Golovach <jenmorgan93@gmail.com>
2017-02-04 20:18:28 +0300

    getCurrentPlayerSymbol added

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,13 +1,23 @@
 class TicTacToe {
     constructor() {
-
+        this.symbol=null;
+        this.turn=1;
     }
 
     getCurrentPlayerSymbol() {
-
-    }
+        if (this.turn%2===0) {
+            this.symbol='o';
+            }
+            else {
+                this.symbol= 'x';
+            }
+            return this.symbol;
+        }
+        
 
     nextTurn(rowIndex, columnIndex) {
+        this.turn++;
+
 
     }
 

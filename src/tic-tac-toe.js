e3e0237f7899adf9522f536e7a867ece1fb62eb2
Julia Golovach <jenmorgan93@gmail.com>
2017-05-06 18:25:23 +0300

    tic-tac-toe game finished

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,171 +1,73 @@
 class TicTacToe {
     constructor() {
-        this.array=[];
+        this.array=[[null,null,null],[null,null,null],[null,null,null]];
         this.symbol='x';
         this.turn=1;
+        this.winner=null;
     }
 
     getCurrentPlayerSymbol() {
-        if (this.turn%2==0) {
-            this.symbol='o';
-            }
-            else {
-                this.symbol= 'x';
-            }
-            return this.symbol;
-            this.turn++;
-        }
+        //should return correct player symbol
+        if (this.turn%2===0) this.symbol='o'
+        else this.symbol= 'x';
+        return this.symbol;
+    }
         
 
     nextTurn(rowIndex, columnIndex) {
+        //should update game state correctly
+        if (!this.array[rowIndex][columnIndex]) {
         this.array[rowIndex][columnIndex]=this.symbol;
         this.turn++;
+        this.getCurrentPlayerSymbol();
+        }
     }
 
     isFinished() {
-        var winner=null;
-        var a=0;
-        for (var i=0;i<4;i++) {
-            for (var j=0;j<4;j++) {
-                if (this.array[i][j]!=null) {
-                    a++;
-                }
-                if (a===3) {
-                    if (this.array[0][0]==this.array[0][1] && this.array[0[0]==this.array[0][2]]){
-                        winner=this.array[0][0];
-                    }
-                    else if (this.array[1][0]==this.array[1][1] && this.array[1][0]==this.array[1][2]) {
-                        winner=this.array[1][0];
-                    }
-                    else if (this.array[2][0]==this.array[2][1] && this.array[2][0]==this.array[2][2]) {
-                        winner=this.array[2][0];
-                    }
-                    else if (this.array[0][0]==this.array[1][0] && this.array[0][0]==this.array[2][0]) {
-                        winner=this.array[0][0];
-                }
-                else if (this.array[0][1]==this.array[1][1] && this.array[0][1]==this.array[2][1]) {
-                        winner=this.array[0][1];
-                }
-                else if (this.array[0][2]==this.array[1][2] && this.array[0][2]==this.array[2][2]) {
-                        winner=this.array[0][2];
-                }
-                else if (this.array[0][0]==this.array[1][1] && this.array[0][0]==this.array[2][2]) {
-                        winner=this.array[0][0];
-                }
-                else if (this.array[0][2]==this.array[1][1] && this.array[0][2]==this.array[2][0]) {
-                        winner=this.array[0][2];
-                }
-                else {
-                    winner=null;
-                }
-                return winner;
-            }
-        }
-        }
+        //should return winner of the game or null
+        if (this.getWinner()||this.isDraw()) return true; 
+        else  return false;
     }
 
     getWinner() {
-         var winner=null;
-        var a=0;
-        for (var i=0;i<4;i++) {
-            for (var j=0;j<4;j++) {
-                if (this.array[i][j]!=null) {
-                    a++;
-                }
-                if (a===3) {
-                    if (this.array[0][0]==this.array[0][1] && this.array[0[0]==this.array[0][2]]){
-                        winner=this.array[0][0];
-                    }
-                    else if (this.array[1][0]==this.array[1][1] && this.array[1][0]==this.array[1][2]) {
-                        winner=this.array[1][0];
-                    }
-                    else if (this.array[2][0]==this.array[2][1] && this.array[2][0]==this.array[2][2]) {
-                        winner=this.array[2][0];
-                    }
-                    else if (this.array[0][0]==this.array[1][0] && this.array[0][0]==this.array[2][0]) {
-                        winner=this.array[0][0];
-                }
-                else if (this.array[0][1]==this.array[1][1] && this.array[0][1]==this.array[2][1]) {
-                        winner=this.array[0][1];
-                }
-                else if (this.array[0][2]==this.array[1][2] && this.array[0][2]==this.array[2][2]) {
-                        winner=this.array[0][2];
-                }
-                else if (this.array[0][0]==this.array[1][1] && this.array[0][0]==this.array[2][2]) {
-                        winner=this.array[0][0];
-                }
-                else if (this.array[0][2]==this.array[1][1] && this.array[0][2]==this.array[2][0]) {
-                        winner=this.array[0][2];
-                }
-                else {
-                    winner=null;
-                }
-                return winner;
-            }
-            else {return null;}
-        }
-        }
+        //should return winner of the game or null
+        if ((this.array[0][0]==='x'&&this.array[0][1]==='x'&&this.array[0][2]==='x')
+        ||(this.array[1][0]==='x'&&this.array[1][1]==='x'&&this.array[1][2]==='x')
+        ||(this.array[2][0]==='x'&&this.array[2][1]==='x'&&this.array[2][2]==='x')
+        ||(this.array[0][0]==='x'&&this.array[1][0]==='x'&&this.array[2][0]==='x')
+        ||(this.array[0][1]==='x'&&this.array[1][1]==='x'&&this.array[2][1]==='x')
+        ||(this.array[0][2]==='x'&&this.array[1][2]==='x'&&this.array[2][2]==='x')
+        ||(this.array[0][0]==='x'&&this.array[1][1]==='x'&&this.array[2][2]==='x')
+        ||(this.array[2][0]==='x'&&this.array[1][1]==='x'&&this.array[0][2]==='x')
+        ) return 'x'
+        else if ((this.array[0][0]==='o'&&this.array[0][1]==='o'&&this.array[0][2]==='o')
+        ||(this.array[1][0]==='o'&&this.array[1][1]==='o'&&this.array[1][2]==='o')
+        ||(this.array[2][0]==='o'&&this.array[2][1]==='o'&&this.array[2][2]==='o')
+        ||(this.array[0][0]==='o'&&this.array[1][0]==='o'&&this.array[2][0]==='o')
+        ||(this.array[0][1]==='o'&&this.array[1][1]==='o'&&this.array[2][1]==='o')
+        ||(this.array[0][2]==='o'&&this.array[1][2]==='o'&&this.array[2][2]==='o')
+        ||(this.array[0][0]==='o'&&this.array[1][1]==='o'&&this.array[2][2]==='o')
+        ||(this.array[2][0]==='o'&&this.array[1][1]==='o'&&this.array[0][2]==='o')
+        ) return 'o'
+        else return null;
+
     }
 
     noMoreTurns() {
-        var winner=null;
-        var a=0;
-        for (var i=0;i<4;i++) {
-            for (var j=0;j<4;j++) {
-                if (this.array[i][j]!=null) {
-                    a++;
-                }
-                if (a===3) {
-                    return true;
-                }
-                else {false;}
-    }
-        }
+        //should true if game field is full and false otherwise
+        if (this.turn===10) return true;
+        else return false;
     }
 
     isDraw() {
-        var winner=null;
-        var a=0;
-        for (var i=0;i<4;i++) {
-            for (var j=0;j<4;j++) {
-                if (this.array[i][j]!=null) {
-                    a++;
-                }
-                if (a===3) {
-                    if (this.array[0][0]==this.array[0][1] && this.array[0[0]==this.array[0][2]]){
-                        return false;
-                    }
-                    else if (this.array[1][0]==this.array[1][1] && this.array[1][0]==this.array[1][2]) {
-                        return false;
-                    }
-                    else if (this.array[2][0]==this.array[2][1] && this.array[2][0]==this.array[2][2]) {
-                        return false;
-                    }
-                    else if (this.array[0][0]==this.array[1][0] && this.array[0][0]==this.array[2][0]) {
-                        return false;
-                }
-                else if (this.array[0][1]==this.array[1][1] && this.array[0][1]==this.array[2][1]) {
-                        return false;
-                }
-                else if (this.array[0][2]==this.array[1][2] && this.array[0][2]==this.array[2][2]) {
-                        return false;
-                }
-                else if (this.array[0][0]==this.array[1][1] && this.array[0][0]==this.array[2][2]) {
-                        return false;
-                }
-                else if (this.array[0][2]==this.array[1][1] && this.array[0][2]==this.array[2][0]) {
-                        return false;
-                }
-                else {
-                    return true;
-                }
-            }
-            else {return false;}
-        }
-        }
+        // should return false if game is not finished or there is a winner 
+        //and true if it is a draw
+        if ((this.noMoreTurns())&&!(this.getWinner())) return true;
+        else return false;
     }
 
     getFieldValue(rowIndex, colIndex) {
+        //should return correct field value
         return this.array[rowIndex][colIndex];
     }
 }
